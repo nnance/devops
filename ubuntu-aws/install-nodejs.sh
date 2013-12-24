c03febdc3d173db8fd8ab1d5089dea4ab56121fa
Nick Nance <nicknance@Nicks-MacBook-Pro-15i.local>
2013-12-24 09:39:34 -0600

    adding build utilities

--- ubuntu-aws/install-nodejs.sh
+++ ubuntu-aws/install-nodejs.sh
@@ -3,5 +3,6 @@ sudo apt-get install python-software-properties python g++ make
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install nodejs
+sudo npm install -g grunt
 sudo npm install -g forever
 mkdir apps
