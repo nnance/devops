477ab3637d5c27fdddb089c3b67ac7a504c60c24
Nick Nance <nicknance@Nicks-MacBook-Pro-15i.local>
2013-12-24 09:14:50 -0600

    a few improvements

--- ubuntu-aws/install-nodejs.sh
+++ ubuntu-aws/install-nodejs.sh
@@ -3,3 +3,5 @@ sudo apt-get install python-software-properties python g++ make
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install nodejs
+sudo npm install -g forever
+mkdir apps
