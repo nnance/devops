b8e309d11b8b1854909598084110044e4aa8e098
Nick Nance <nance.nick@gmail.com>
2014-07-20 18:57:39 -0500

    update sass install

--- ubuntu-aws/install-nodejs-build-tools.sh
+++ ubuntu-aws/install-nodejs-build-tools.sh
@@ -1,4 +1,5 @@
 sudo npm install -g bower grunt-cli grunt
-sudo apt-get -y install ruby-full
-sudo gem install sass
-sudo gem install compass
+sudo apt-get -y install ruby-sass
+sudo apt-get -y install ruby-compass
+#sudo gem install sass
+#sudo gem install compass
