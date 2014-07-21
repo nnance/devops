2cd50f0bfa71b91f352abb7df20c414407472380
Nick Nance <nance.nick@gmail.com>
2014-07-20 21:51:33 -0500

    fix for installing mongodb

--- ubuntu-aws/install-mongodb.sh
+++ ubuntu-aws/install-mongodb.sh
@@ -1,5 +1,5 @@
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get -y update
 sudo apt-get -y install mongodb-10gen
 sudo service mongodb stop
