db630584fe0339f3c3ed5e49eda88a8717b61236
Nick Nance <nance.nick@gmail.com>
2014-07-20 21:26:50 -0500

    changing ruby install to be compatible with ubuntu trusty image

--- ubuntu-aws/install-nodejs-build-tools.sh
+++ ubuntu-aws/install-nodejs-build-tools.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get -y install ruby1.9.3
+sudo apt-get -y install ruby-full
 sudo gem install sass
 sudo gem install compass
-#sudo npm install -g bower grunt-cli grunt
+sudo npm install -g bower grunt-cli grunt
