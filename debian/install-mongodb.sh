7c980fced33ac772686c4d3b6904e9aad5c6d3ad
Nick Nance <nance.nick@gmail.com>
2015-02-03 22:13:51 -0600

    added debian support

---
+++ debian/install-mongodb.sh
@@ -0,0 +1,6 @@
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org=2.6.1 mongodb-org-server=2.6.1 mongodb-org-shell=2.6.1 mongodb-org-mongos=2.6.1 mongodb-org-tools=2.6.1
+sudo service mongodb stop
+sudo service mongodb start
---
+++ debian/install-nodejs.sh
@@ -0,0 +1,4 @@
+apt-get install curl
+curl -sL https://deb.nodesource.com/setup | bash -
+apt-get install -y nodejs
+apt-get install -y build-essential
